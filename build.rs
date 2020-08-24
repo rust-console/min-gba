7a2c1b92b8d1890524d062da56346499acc33e4b
Lokathor <zefria@gmail.com>
2020-08-24 17:15:33 -0600

    improve build/linking for better downstream usage.

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 fn main() {
+  let out_file = "rsrt0.o";
+  let out_dir = std::env::var("OUT_DIR").unwrap();
+  let out_dir_file = format!("{}/{}", out_dir, out_file);
   let as_output = std::process::Command::new("arm-none-eabi-as")
-    .args(&["-o", "target/rsrt0.o"])
+    .args(&["-o", out_dir_file.as_str()])
     .arg("-mthumb-interwork")
     .arg("-mcpu=arm7tdmi")
     .arg("src/rsrt0.S")
@@ -9,4 +12,7 @@ fn main() {
   if !as_output.status.success() {
     panic!("{}", String::from_utf8_lossy(&as_output.stderr));
   }
+  //
+  println!("cargo:rustc-link-search={}", out_dir);
+  println!("cargo:rustc-link-lib={}", out_file);
 }
